bfd5c75ede13cb84dc4e443499593d30200ea7af
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2020-03-03 11:19:26 +0100

    Create MultipleConditionalBlock.py

---
+++ MultipleConditionalBlock.py
@@ -0,0 +1,10 @@
+message = "hello there"
+ 
+if "hello" in message:
+    print("hi")
+elif "hi" in message:
+    print("hi")
+elif "hey" in message:
+    print("hi")
+else:
+    print("I don't understand")
