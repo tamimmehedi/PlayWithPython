4441e99893f5178a909f7e3fc284406cf05c89d7
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2020-03-04 15:19:46 +0100

    Create StringFormattingWithDictLoop.py

---
+++ StringFormattingWithDictLoop.py
@@ -0,0 +1,12 @@
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for pair in phone_numbers.items():
+    print("{} has as phone number {}".format(pair[0], pair[1]))
+    
+    OR,
+    
+    
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for key, value in phone_numbers.items():
+    print("{} has as phone number {}".format(key, value))
