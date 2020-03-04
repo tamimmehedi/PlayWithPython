1e72886324c6db2ae71965cdd30353c131381152
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2020-03-04 10:54:17 +0100

    Create StringFormattingWithMultipleVariables.py

---
+++ StringFormattingWithMultipleVariables.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+surname = input("Enter your name: ")
+message = ("hello %s %s") %(name,surname)
+print(message)
+
+       OR,
+       
+name = input("Enter your name: ")
+surname = input("Enter your name: ")
+message = f"hello {name} {surname}"
+print(message)
