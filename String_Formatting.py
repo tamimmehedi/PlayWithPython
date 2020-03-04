950c2644b5dbdc39c5615cc6b5e31b8834fedaff
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2020-03-04 10:42:55 +0100

    Update String_Formatting.py

--- String_Formatting.py
+++ String_Formatting.py
@@ -1,3 +1,11 @@
+There are two ways of doing it:-
+  
 user_input = input("Enter your Name: ")
 message = f"Hello {user_input}"
 print(message)
+
+        OR,
+user_input = input("Enter your Name: ")
+message = "Hello %s" %user_input
+print(message)
+  
